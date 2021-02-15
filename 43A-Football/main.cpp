26cbeb42459f0b29c43d29c438a788c938b5b472
Florian Rudolf <rudolf@hs-ulm.de>
2021-02-15 22:07:12 +0100

    gitignore added

--- 43A-Football/main.cpp
+++ 43A-Football/main.cpp
@@ -1,10 +1,37 @@
 #include <bits/stdc++.h>
 
-
 int main()
 {
+    int numberOfLines = 0;
+    std::cin >> numberOfLines;
+    std::vector<std::string> goals;
 
+    std::string teamName_1;
+    std::string teamName_2;
+    int score_1 = 0;
+    int score_2 = 0;
 
+    for (int i = 0; i < numberOfLines; i++)
+    {
+        std::cin >> teamName_1;
+        goals.push_back(teamName_1);
+    }
+    teamName_1 = goals[0];
+
+    while (numberOfLines--)
+    {
+        if (goals[numberOfLines] != teamName_1)
+        {
+            score_2++;
+            teamName_2 = goals[numberOfLines];
+        }
+        else
+        {
+            score_1++;
+        }
+    }
+
+    std::cout << ((score_1>score_2) ? teamName_1 : teamName_2) << std::endl;
 
     return 0;
 }
